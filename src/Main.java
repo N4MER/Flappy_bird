44c91090420423cdb88fe32dc2d8cb075326eab4
N4MER <yangzhaoxiang@seznam.cz>
2024-06-03 10:49:10 +0200

    -pls let me

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import UI.FlappyBird;
 
+import java.io.IOException;
+
+
 public class Main {
     public static void main(String[] args) {
-
         new FlappyBird(
-                "src/flappyBirdImages/background.png",
-                "src/flappyBirdImages/flappyBird.png",
-                "src/flappyBirdImages/flappyBirdFalling.png",
-                "src/flappyBirdImages/flappyBirdSemiFalling.png",
-                "src/flappyBirdImages/flappyBirdGoingUp.png",
-                "src/flappyBirdImages/bottomPipe.png",
-                "src/flappyBirdImages/topPipe.png"
+                "./src/flappyBirdImages/background.png",
+                "./src/flappyBirdImages/flappyBird.png",
+                "./src/flappyBirdImages/flappyBirdFalling.png",
+                "./src/flappyBirdImages/flappyBirdSemiFalling.png",
+                "./src/flappyBirdImages/flappyBirdGoingUp.png",
+                "./src/flappyBirdImages/bottomPipe.png",
+                "./src/flappyBirdImages/topPipe.png"
         );
+
 
     }
 }
